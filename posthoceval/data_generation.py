f4d2750b6ffbbb46b8b792d81919690a7a01201a
Zach Carmichael <20629897+craymichael@users.noreply.github.com>
2020-12-28 16:52:09 -0500

    debug

--- posthoceval/data_generation.py
+++ posthoceval/data_generation.py
@@ -76,8 +76,15 @@ def sample(variables, distribution, n_samples, constraints=None, cov=None,
             # TODO: note that sympy==1.6 is necessary, there is a non-public
             #  regression for some expressions in 1.7
             #  https://github.com/sympy/sympy/issues/20563
-            from sympy.stats.rv import random_symbols
-            print(d, *args, random_symbols(args[0]), args[0].free_symbols, d.free_symbols, d.pspace, d.pspace.values)
+            from sympy.stats.rv import random_symbols, pspace
+            from sympy import Tuple
+            print()
+            print('WHY')
+            thing = Tuple(d, args[0])
+            print(thing)
+            print(random_symbols(thing))
+            print(thing.free_symbols)
+            print(pspace(thing))
             try:
                 samples = sp.stats.sample_iter(d, *args)
             except NameError:
