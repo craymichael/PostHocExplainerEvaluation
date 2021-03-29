cb175f9954089b1472521d7caec18c0366a89126
Zach Carmichael <20629897+craymichael@users.noreply.github.com>
2021-03-29 15:34:56 -0400

    working lime for classification, probably

--- posthoceval/explainers/local/lime.py
+++ posthoceval/explainers/local/lime.py
@@ -9,7 +9,7 @@ from typing import Union
 
 import numpy as np
 
-from interpret.blackbox import LimeTabular
+from lime.lime_tabular import LimeTabularExplainer
 
 from posthoceval.profile import profile
 from posthoceval.explainers._base import BaseExplainer
@@ -31,16 +31,12 @@ class LIMEExplainer(BaseExplainer):
         self.model = model
         self.verbose = verbose
         self.seed = seed
-        if task != 'regression':
-            # you'd think that this wrapper would make things easier...
-            raise NotImplementedError('Backend `interpret` does not have '
-                                      'proper classification support....')
         self.task = task
 
         self.num_samples = num_samples
 
         # will be initialized in fit
-        self._explainer = None
+        self._explainer: Optional[LimeTabularExplainer] = None
 
         self._mean = None
         self._scale = None
@@ -49,23 +45,43 @@ class LIMEExplainer(BaseExplainer):
         if self.verbose > 0:
             logger.info('Fitting LIME')
 
-        self._explainer = LimeTabular(
-            predict_fn=self.model,
-            data=X,
+        # TODO:
+        #  categorical_features: list of indices (ints) corresponding to the
+        #    categorical columns. Everything else will be considered
+        #    continuous. Values in these columns MUST be integers.
+        #  categorical_names: map from int to list of names, where
+        #    categorical_names[x][y] represents the name of the yth value of
+        #    column x.
+
+        self._explainer = LimeTabularExplainer(
+            training_data=X,
             feature_names=range(self.model.n_features),
             mode=self.task,
             random_state=self.seed,
             discretize_continuous=False,
-            explain_kwargs={'num_features': X.shape[1],
-                            'num_samples': self.num_samples}
         )
 
         # used in un-normalizing contributions
-        self._mean = self._explainer.lime.scaler.mean_
-        self._scale = self._explainer.lime.scaler.scale_
+        self._mean = self._explainer.scaler.mean_
+        self._scale = self._explainer.scaler.scale_
 
     def predict(self, X):
         pass  # TODO: n/a
+
+    def _process_explanation(self, expl_vals_i, xi, intercept_i=None):
+        # sort explanation values...
+        _, coefs_i = zip(*sorted(expl_vals_i, key=lambda x: x[0]))
+        # LIME scales with z-score (StandardScaler)
+        coefs_i = np.asarray(coefs_i) / self._scale
+        # feature-wise contributions to output
+        contribs_i = coefs_i * xi
+
+        # if desired, intercept can be fetched and adjusted
+        if intercept_i is not None:
+            intercept_i = (intercept_i -
+                           np.sum(coefs_i * self._mean))
+            return contribs_i, intercept_i
+        return contribs_i
 
     @profile
     def feature_contributions(self, X: np.ndarray, as_dict=False,
@@ -80,58 +96,87 @@ class LIMEExplainer(BaseExplainer):
         if self.verbose > 0:
             logger.info('Fetching LIME explanations')
 
-        # passes explain_kwargs from LIME init to LIME explain_instance
-        explanation = self._explainer.explain_local(X)
-
         contribs_lime = []
         intercepts = []
 
+        explain_kwargs = {
+            'predict_fn': self.model,
+            'num_features': X.shape[-1],
+            'num_samples': self.num_samples,
+        }
+        if self.task == 'classification':
+            # get number of classes, obtain feature contributions for each
+            #  class
+            explain_kwargs['top_labels'] = self.model(X[:1]).shape[-1]
+
         for i, xi in enumerate(X):
-            expl_i = explanation.data(i)
+            expl_i = self._explainer.explain_instance(xi, **explain_kwargs)
+            expl_i_map = expl_i.as_map()
 
-            # sort explanation values...
-            expl_names = expl_i['names']  # feature indices
-            expl_scores = expl_i['scores']  # unsorted coefficients
-            # get all items and sort them to match order of features
-            coefs_i, _ = zip(*sorted(zip(expl_scores, range(len(expl_names))),
-                                     key=lambda x: expl_names[x[1]]))
-            # LIME scales with z-score (StandardScaler)
-            coefs_i = np.asarray(coefs_i) / self._scale
+            if self.task == 'classification':
+                contribs_i = []
+                intercept_i = []
+                for k in range(explain_kwargs['top_labels']):
+                    # explanation coefs for class k
+                    expl_ik = expl_i_map[k]
 
-            # if desired, intercept can be fetched and adjusted via:
-            if return_intercepts:
-                intercept = (expl_i['extra']['scores'][0] -
-                             np.sum(coefs_i * self._mean))
-                intercepts.append(intercept)
+                    if return_intercepts:
+                        contribs_ik, intercept_ik = self._process_explanation(
+                            expl_ik, xi, intercept_i=expl_i.intercept[k])
+                        intercept_i.append(intercept_ik)
+                    else:
+                        contribs_ik = self._process_explanation(expl_ik, xi)
 
-            contribs_i = coefs_i * xi
+                    contribs_i.append(contribs_ik)
+            else:
+                expl_i = expl_i_map[1]
+
+                if return_intercepts:
+                    contribs_i, intercept_i = self._process_explanation(
+                        expl_i, xi, intercept_i=expl_i.intercept[1])
+                else:
+                    contribs_i = self._process_explanation(expl_i, xi)
+
+            # store contributions and maybe intercept(s)
             contribs_lime.append(contribs_i)
+            if return_intercepts:
+                # noinspection PyUnboundLocalVariable
+                intercepts.append(intercept_i)
 
         contribs_lime = np.asarray(contribs_lime)
+
+        if self.task == 'classification':
+            # samples x classes x features --> classes x samples x features
+            contribs_lime = np.moveaxis(contribs_lime, 0, 1)
+
         # TODO: move this logic to super? likely redundant between explainers
         if as_dict:
             contribs_lime = dict(zip(self.model.symbols, contribs_lime.T))
 
         if return_intercepts:
+            intercepts = np.asarray(intercepts)
+            if self.task == 'classification':
+                # samples x classes --> classes x samples
+                intercepts = intercepts.T
             return contribs_lime, intercepts
 
         return contribs_lime
+
 
 # aids
 
 # TODO: Make kwargs explicit.
 class LimeTabular(ExplainerMixin):
-
     available_explanations = ["local"]
     explainer_type = "blackbox"
 
     def __init__(
-        self,
-        predict_fn,
-        data,
-        feature_names=None,
-        feature_types=None,
-        **kwargs
+            self,
+            predict_fn,
+            data,
+            feature_names=None,
+            feature_types=None,
+            **kwargs
     ):
         """ Initializes class.
         Args:
@@ -192,7 +237,8 @@ class LimeTabular(ExplainerMixin):
             data_dict = {
                 "names": names,
                 "scores": scores,
-                "extra": {"names": ["Intercept"], "scores": [intercept], "values": [1]},
+                "extra": {"names": ["Intercept"], "scores": [intercept],
+                          "values": [1]},
             }
             data_dicts.append(data_dict)
 
