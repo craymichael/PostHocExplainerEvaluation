fcde42220a5798037e4162e1812dafcfd503a2c0
Zach Carmichael <20629897+craymichael@users.noreply.github.com>
2021-05-26 13:16:01 -0400

    fix maple pred stack

--- posthoceval/explainers/local/maple.py
+++ posthoceval/explainers/local/maple.py
@@ -287,7 +287,7 @@ class MAPLEExplainer(BaseExplainer):
         if self.task == 'classification':
             contribs_maple = np.moveaxis(contribs_maple, 0, 1)
 
-        y_maple = np.concatenate(y_maple, axis=0)
+        y_maple = np.stack(y_maple)
         if self.task == 'regression':
             y_maple = y_maple.squeeze(axis=1)
 
