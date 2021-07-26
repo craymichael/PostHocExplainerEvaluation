e8a34febe88eaea3dd5b4af215f9337a75083552
Zach Carmichael <20629897+craymichael@users.noreply.github.com>
2021-07-26 18:27:42 -0400

    print --> warning

--- posthoceval/models/synthetic.py
+++ posthoceval/models/synthetic.py
@@ -327,9 +327,10 @@ def generate_additive_expression(
     io_orig = interaction_ord
     interaction_ord = tuple(io for io in interaction_ord if io <= n_uniq_main)
     if io_orig != interaction_ord:
-        print(f'Warning: provided interaction_ord {io_orig} contains '
-              f'interaction orders too large for n_uniq_main ({n_uniq_main}). '
-              f'Using these orders instead: {interaction_ord}')
+        warnings.warn(f'Warning: provided interaction_ord {io_orig} contains '
+                      f'interaction orders too large for n_uniq_main '
+                      f'({n_uniq_main}). Using these orders instead: '
+                      f'{interaction_ord}')
 
     # Compute the possible number of unique interactions that are possible
     possible_int_ords = {
